3f26916067520d37da3e4362b7b389d275fcf21d
Gabriel Cintra <gabriel.cintra@gmail.com>
2013-11-15 22:09:38 -0200

    initial files

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+/* ==========================================================================
+   Author:      YOUR NAME ()
+   Version:     1.0.0
+   Create:      15/11/2013
+
+   Revision:    
+   Last Mod.:   15/11/2013
+========================================================================== */
+
+/* -----------------------------------
+    $Onload
+----------------------------------- */
+$(function(){
+
+
+
+
+/* -----------------------------------
+  NiceScroll
+  http://areaaperta.com/nicescroll/
+----------------------------------- */
+/* Exemplo de aplicação:
+$("html").niceScroll({styler:"fb",cursorcolor:"red"});
+$(".rules-box").niceScroll({styler:"fb",cursorcolor:"red"});
+*/
+
+
+/* -----------------------------------
+  $ Mask input 
+  Criar tipos de máscara aqui   
+  ----------------------------------- */
+  /*
+  Exemplos    
+  $(".phone-type").mask("(99) 9999-9999?9"); // telefone com ou sem nono digito
+  $(".cpf-type").mask("999.999.999-99"); 
+  $(".rg-type").mask("99.999.999-9");
+  */
+
+/* -----------------------------------  
+    $ Correção do placeholder para IE    
+    ----------------------------------- */
+    $('input, textarea').placeholder();
+
+
+
+    
+});
+//fim on load
+
+
+
